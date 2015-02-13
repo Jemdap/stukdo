0fcf8863226774563f817e5ba468d4e6d469537d
Jemond Patterson <pattersonjemond@gmail.com>
2015-02-13 03:40:20 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stukdo::Application.config.secret_token = 'd725317c762ce0e6d7b7bec409f7053e1d506ef9cf4233fcbf9d5ed15f65cc48ad4d8aba495e40897f127442b0938acd67055b5b4e29624f3c02361b646de6b3'
