3b4bbc3ae723f9d06d99dbbd388668ef704128a1
Jemond Patterson <pattersonjemond@gmail.com>
2015-02-13 10:26:36 -0500

    Added the about page

--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,8 @@ Stukdo::Application.routes.draw do
   # just remember to delete public/index.html.
    root :to => 'pages#home'
 
+   get 'about' => "pages#home"
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
